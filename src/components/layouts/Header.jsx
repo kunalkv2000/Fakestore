50a3b1ff5aae593429221d1522b52541241401ac
Kunal K V <kunalvijayakumar.021200@gmail.com>
2025-03-03 09:43:02 +0530

    Update Header.jsx

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="flex justify-between items-center px-14 md-auto py-3 sticky top-0 z-10 bg-black-5 dark:bg-white-10  ">
       <div>
-        <img src='./src/assets/logo.png' className='w-auto h-10'/>
+        <img src='https://github.com/kunalkv2000/Fakestore/blob/main/src/assets/logo.png' className='w-auto h-10'/>
       </div>
       <div className="space-x-4">
         <CartModal />
